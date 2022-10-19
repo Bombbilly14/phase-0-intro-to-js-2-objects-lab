6b810f55d611adc746eb5637d2b7a04d3bfeffba
William German <william.j.german2@gmail.com>
2022-10-18 17:03:56 -0700

    commit tree f0aa5faf48dde5da4570fc70351db375f8a0fbde

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "William",
+    streetAddress: "114 e",
+}
+function updateEmployeeWithKeyAndValue(employee, name, value){
+    const newEmployee={...employee}
+    newEmployee[name]=value
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, value){
+    employee[name]= value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newerEmployee={...employee}
+    delete newerEmployee.name
+    return newerEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name
+    return employee
+}
